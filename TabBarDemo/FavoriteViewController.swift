46c9af2de29b833e7449c19e2d5d8ee093ac509e
adeluleye <adelu4real@gmail.com>
2018-10-08 14:04:20 +0100

    add video and favorite controller for demo purpose

---
+++ TabBarDemo/FavoriteViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteViewController.swift
+//  TabBarDemo
+//
+//  Created by ADELU ABIDEEN ADELEYE on 10/8/18.
+//  Copyright © 2018 Spantom Technologies Ltd. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
