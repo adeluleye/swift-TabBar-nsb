4b24007b7f45c52c4d44ca3b436cfb80f769c294
adeluleye <adelu4real@gmail.com>
2018-10-08 16:07:30 +0100

    setup video and favorite view in the tab bar

--- TabBarDemo/MainTabBarController.swift
+++ TabBarDemo/MainTabBarController.swift
@@ -12,7 +12,21 @@ class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tabBar.barTintColor = UIColor(red: 38/255, green: 196/255, blue: 133/255, alpha: 1)
+    }
+    
+    func setupTabBar() {
+        
+        let videoViewController = UINavigationController(rootViewController: VideoViewController())
+        videoViewController.tabBarItem.image = UIImage(named: "video_black")
+        videoViewController.tabBarItem.selectedImage = UIImage(named: "video_white")
+        
+        let favoriteViewController = UINavigationController(rootViewController: FavoriteViewController())
+        favoriteViewController.tabBarItem.image = UIImage(named: "star_black")
+        favoriteViewController.tabBarItem.selectedImage = UIImage(named: "star_white")
+        
+        viewControllers = [videoViewController, favoriteViewController]
     }
 
 
